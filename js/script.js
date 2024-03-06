0728fbc8c2f74d8f08b147c36bd94c8e8b4df88a
HardWorkers2 <accwebdmytro38140@gmail.com>
2024-03-06 16:39:18 +0200

    Landing "Partnero" without media-css and js script

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// const button = document.getElementById('trial');
+// const item = document.getElementById('plan-item');
+
+// item.addEventListener('mouseover', function () {button.classList.toggle('white')});
