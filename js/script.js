779be69799e52c244490fc8666b3a6561bb71834
HardWorkers2 <accwebdmytro38140@gmail.com>
2024-03-14 19:33:04 +0200

    Complete all media-query. Create menu hamburger

--- js/script.js
+++
@@ -1,4 +0,0 @@
-// const button = document.getElementById('trial');
-// const item = document.getElementById('plan-item');
-
-// item.addEventListener('mouseover', function () {button.classList.toggle('white')});
